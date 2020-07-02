08da0f06a8f12fb3f757e5061f1c0c9900a47254
ShiinaMashino <514677915@qq.com>
2020-07-02 13:19:42 +0800

    2.4  远程相机 打包bug

---
+++ src/views/VideoTest.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-button @click="start">start</el-button>
+    <el-button @click="stop">stop</el-button>
+    <video id="testVideo"></video>
+  </div>
+</template>
+
+<script>
+  let that
+  export default {
+    name: "VideoTest",
+    data() {
+      return {
+        video: null,
+        stream: null,
+        open: () => {
+          console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        }
+      }
+    },
+    methods: {
+      start() {
+        navigator.mediaDevices.getUserMedia({audio: true, video: {width: 360, height: 640}}).then(stream => {
+          this.stream = stream
+          this.video.srcObject = stream
+          this.video.play()
+        }).catch(res => {
+          console.log('fail: ')
+          console.log(res)
+        })
+      },
+      stop() {
+        console.log(this.stream)
+      }
+    },
+    beforeCreate() {
+      that = this
+    },
+    mounted() {
+      this.video = document.getElementById('testVideo')
+      this.open()
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
